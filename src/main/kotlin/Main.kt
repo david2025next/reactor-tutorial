cc6d422694f6c5876740a46755b188db57c767e7
Roman Elizarov <davidayeledoux@gmail.com>
2026-01-20 11:39:46 +0100

    subscribe with error, complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package org.next.roman
 
+import reactor.kotlin.core.publisher.toFlux
+
 fun main() {
 
+    (1..4).toFlux()
+        .map { if (it <= 3) return@map it else throw RuntimeException("Go to 4") }
+        .subscribe({ println(it) }, { System.err.println("Error $it") }, { println("Done") })
 }
